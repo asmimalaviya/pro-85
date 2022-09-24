b42a6af64683111b566880d51845b3d7dd5d818c
asmimalaviya <86877268+asmimalaviya@users.noreply.github.com>
2022-09-24 15:10:12 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+      apiKey: "AIzaSyBHzgRP1nKydG7DZAL1moir_Sjz-xS_5EM",
+  authDomain: "pro-85-b6d48.firebaseapp.com",
+  projectId: "pro-85-b6d48",
+  storageBucket: "pro-85-b6d48.appspot.com",
+  messagingSenderId: "906953569445",
+  appId: "1:906953569445:web:5c3ea0d305be1f5433894c"
+  };
